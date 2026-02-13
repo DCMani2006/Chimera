9e5bddd5d6b8e298e76ba2530031833dbc3c2c7c
DCMani2006 <rhyscassaz@gmsil.com>
2026-02-13 17:42:27 +0530

    Remove static frontend serving for production

--- src/app.js
+++ src/app.js
@@ -11,10 +11,5 @@ app.use(express.json());
 
 app.use("/api/world", worldRoutes);
 
-app.use(express.static(path.join(__dirname, "../frontend")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "index.html"));
-});
 
 module.exports = app;
