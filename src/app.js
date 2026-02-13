f75e16d07671cbed61702d6aae801d6574bec006
DCMani2006 <rhyscassaz@gmsil.com>
2026-02-13 17:25:24 +0530

    Mount world routes

--- src/app.js
+++ src/app.js
@@ -2,24 +2,19 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
+const worldRoutes = require("./routes/worldRoutes");
+
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Serve frontend
+app.use("/api/world", worldRoutes);
+
 app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend", "index.html"));
 });
-
-// Example test route (to confirm backend works)
-app.get("/api/test", (req, res) => {
-  res.json({ message: "Backend is working!" });
-});
-const worldRoutes = require("./routes/worldRoutes");
-app.use("/api/world", worldRoutes);
 
 module.exports = app;
